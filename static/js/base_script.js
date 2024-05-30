1d9a0381cc87c17510672f29976138f4cbc6b23b
noughtsNcrosses <joeljefferson2004@gmail.com>
2024-05-30 13:20:34 +0530

    add responsive navbar

--- static/js/base_script.js
+++ static/js/base_script.js
@@ -1,1 +1,50 @@
-console.log("testing 1, 2, 3");
+// responsive navbar massacre
+
+let open_ham = document.querySelector('#openham');
+let close_ham = document.querySelector('#closeham');
+let navigation_items = document.querySelector(".navigation-items");
+let button = "open";
+
+let toggle = (open, close, nav) =>{
+    open_ham.style.display = open;
+    close_ham.style.display = close;
+    navigation_items.style.display = nav;
+}
+
+open_ham.addEventListener('click',() => {toggle("none", "inline", "flex"); button="close";});
+close_ham.addEventListener('click', ()=> {toggle("inline", "none", "none"); button="open";});
+
+
+
+function reset(x){
+    console.log("why?")
+    if(x.matches){
+        open_ham.style.display = "none";
+        close_ham.style.display = "none";
+        navigation_items.style.display = "flex";
+        button = "open";
+    }
+}
+
+const x = window.matchMedia("(min-width:769px)");
+x.addEventListener("change", () => reset(x));
+
+function reset2(y){
+    if(y.matches){
+
+        if(button="open"){
+            open_ham.style.display = "inline";
+            close_ham.style.display = "none";
+            navigation_items.style.display = "none";
+
+        }
+        else{
+            open_ham.style.display = "none";
+            close_ham.style.display = "inline";
+            navigation_items.style.display = "flex";
+        }
+    }
+}
+
+const y = window.matchMedia("(max-width:768px)");
+y.addEventListener("change", () => reset2(y));
